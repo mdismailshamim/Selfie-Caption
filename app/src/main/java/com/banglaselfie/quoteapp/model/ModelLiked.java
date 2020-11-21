32bc8d949cb886c5242a60a42f4275a85221addc
mdismailshamim <shamimbd8.net@gmail.com>
2020-11-21 11:21:27 +0600

    Initial commit

---
+++ app/src/main/java/com/banglaselfie/quoteapp/model/ModelLiked.java
@@ -0,0 +1,22 @@
+package com.banglaselfie.quoteapp.model;
+
+public class ModelLiked {
+
+    private String quote;
+
+    public ModelLiked() {
+    }
+
+
+    public ModelLiked(String quote) {
+        this.quote = quote;
+    }
+
+    public String getQuote() {
+        return quote;
+    }
+
+    public void setQuote(String quote) {
+        this.quote = quote;
+    }
+}
